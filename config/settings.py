da5b4d3033801ede8d6824393cd58f66f6836947
Kenn <smail@192.168.11.172>
2026-07-01 21:00:40 +0100

    Fix: Ajout de anymail et configuration de l'API Brevo

--- config/settings.py
+++ config/settings.py
@@ -136,22 +136,18 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+# Configuration de l'envoi d'e-mails via l'API HTTP de Brevo
+INSTALLED_APPS = [
+    # ... gardez vos applications existantes ...
+    'anymail', 
+]
 
-import os
+EMAIL_BACKEND = "anymail.backends.brevo.EmailBackend"
+DEFAULT_FROM_EMAIL = 'yatoutci2@gmail.com'
 
-# Configuration de l'envoi d'e-mails (SMTP Brevo)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = True
-
-# Vos accès de connexion Brevo
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') # 'hfnwcnrnfvhuayohO') 
-
-# Votre expéditeur par défaut (à modifier en contact@yatoutci.com dès que cPanel sera débloqué)
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'Yatout Ci <yatoutci2@gmail.com>')
+ANYMAIL = {
+    "BREVO_API_KEY": os.environ.get('EMAIL_HOST_PASSWORD'),
+}
 
 # Permet de valider les sessions même sur les navigateurs mobiles stricts
 SESSION_COOKIE_SAMESITE = 'Lax'
